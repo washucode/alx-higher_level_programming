44ea7a3bb23c522089775ffb1d62664537acffd1
washucode <estherw8525@gmail.com>
2023-06-06 16:22:39 +0300

    fizz buzz function

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            res = "FizzBuzz"
+        elif num % 3 == 0:
+           res = "Fizz"
+        elif num % 5 == 0:
+           res = "Buzz"
+        else:
+            res = num
+        print("{}".format(res), end=" ")
