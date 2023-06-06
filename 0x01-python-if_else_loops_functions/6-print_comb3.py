0f5051a686294461e652825b019a293770065968
washucode <estherw8525@gmail.com>
2023-06-06 15:28:47 +0300

    change assigment to compare op along other errors

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 for first in range(0, 10):
     for second in range(0, 10):
-        if first = second or fist > second:
+        if first == second or first > second:
             continue
-        else if first == 8 and second == 9:
+        elif first == 8 and second == 9:
             print("{}{}".format(first, second))
         else:
             print("{}{}".format(first, second), end=", ")
