d3fd885214fa5511b06890d532100de45ca2dc84
washucode <estherw8525@gmail.com>
2023-06-14 20:19:42 +0300

    use a adifferent approach to solvr

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,27 +2,22 @@
 
 
 def roman_to_int(roman_string):
-    # intiate dictionary with roman numerals and their values
-    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-              'C': 100, 'D': 500, 'M': 1000}
-
+    # check if roman_string is None or not a string
     if roman_string is None or type(roman_string) is not str:
         return 0
-    # if the string is only one character, return the value of that character
-    if len(roman_string) == 1:
-        for key, value in romans.items():
-            if roman_string == key:
-                return value
-    else:
-        total = 0
-        for i in range(len(roman_string)):
-            for key, value in romans.items():
-                if roman_string[i] == key:
-                    if i + 1 < len(roman_string):
-                        if romans[roman_string[i + 1]] > value:
-                            total -= value
-                        else:
-                            total += value
-                    else:
-                        total += value
-        return total
+    # initiate dictionary with roman numerals
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                 'C': 100, 'D': 500, 'M': 1000}
+    num = 0
+    prev = 0
+    # iterate through roman_stringvixx
+    for i in reversed(roman_string):
+        comp_val = romans.get(i, 0)
+        # if value of current roman numeral is less than previous
+        if comp_val < prev:
+            num -= comp_val
+        # else add value of current roman numeral to num vixx
+        else:
+            num += comp_val
+        prev = comp_val
+    return num 
