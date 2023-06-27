049aad959621392e7d82f6af596018170664bfea
washucode <estherw8525@gmail.com>
2023-06-28 02:23:03 +0300

    reconfigure task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,8 +8,9 @@ class Square:
 
     def __init__(self, size=0):
         """private attribute size that is optional"""
-        self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >=0")
+        else:
+            self.__size = size
