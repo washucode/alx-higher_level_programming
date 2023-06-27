feb6ccd252601a19bee6545880b2abedb885932e
washucode <estherw8525@gmail.com>
2023-06-28 02:17:37 +0300

    TASK 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""This module defines a Square class"""
+
+
+class Square:
+    """This class is a square with a size attribute"""
+
+
+    def __init__(self, size=None):
+        """Initialize class attributes"""
+        self.__size = size
