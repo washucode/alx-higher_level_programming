fbe9a1ce27b62a879f135719cbc7da48a6a80dd1
washucode <estherw8525@gmail.com>
2023-06-29 01:50:51 +0300

    import stderr

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,6 +3,7 @@
 
 def safe_function(fct, *args):
     """a function that executes a function safely."""
+    from sys import stderr
     try:
         return fct(*args)
     except Exception as excpt:
