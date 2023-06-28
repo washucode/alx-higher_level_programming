05cbe511a417f6d00613007c8e365f197fee32d4
washucode <estherw8525@gmail.com>
2023-06-29 01:55:36 +0300

    refactor task 0

--- 0x05-python-exceptions/ 0-safe_print_list.py
+++ 0x05-python-exceptions/ 0-safe_print_list.py
@@ -3,11 +3,12 @@
 
 def safe_print_list(my_list=[], x=0):
     """a function that prints x elements of a list"""
-    try:
-        for e in range(x):
+    i = 0
+    for e in range(x):
+        try:
             print("{}".format(my_list[e]), end="")
-        print()
-        return x
-    except IndexError:
-        print()
-        return e
+            i += 1
+        except IndexError:
+            break
+    print()
+    return i
