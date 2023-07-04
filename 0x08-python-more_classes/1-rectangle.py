a5efcea830da8da67dfa9db2cf2e886eb4fe4031
washucode <estherw8525@gmail.com>
2023-07-05 01:28:07 +0300

    task 1 corrections in setters logic

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,7 +5,7 @@
 
 class Rectangle:
     """class creates private instance attributes width and height
-    Args:
+    Attributes:
         width (int): width of the rectangle
         height (int): height of the rectangle
     """
@@ -37,9 +37,9 @@ class Rectangle:
             height
         """
 
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >=0")
         self.__height = value
 
@@ -65,8 +65,8 @@ class Rectangle:
             width
         """
 
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >=0")
         self.__width = value
