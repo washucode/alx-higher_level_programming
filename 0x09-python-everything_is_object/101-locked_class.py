adda44c59757e2a7b1de400f436a6fc53ad63bd1
washucode <estherw8525@gmail.com>
2023-07-05 04:22:24 +0300

    fix task 30

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,3 +4,18 @@ class LockedClass:
     attributes, except if the new instance attribute is called first_name.
     """
     __slots__ = ['first_name']
+
+    def __init__(self, first_name=""):
+        """__init__ initializes the LockedClass instance.
+        Args:
+            first_name (str): the first name of the LockedClass instance.
+        """
+        self.first_name = first_name
+
+    def __setattr__(self, name, value):
+        """__setattr__ sets the LockedClass instance's attributes.
+        """
+        if name != "first_name":
+            raise AttributeError("'LockedClass' object has no attribute '" +
+                                 name + "'")
+        self.__dict__[name] = value
