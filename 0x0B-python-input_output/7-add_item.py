cb7bed259b37a0ab30b0a3bc606c9f528d37062a
washucode <estherw8525@gmail.com>
2023-07-13 00:57:50 +0300

    change task 7 logic

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,13 +9,12 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
-def add_item(args):
-    """Add all arguments to a Python list, and then save them to a file"""
+if __name__ == "__main__":
     filename = "add_item.json"
     if path.exists(filename):
         pylist = load_from_json_file(filename)
     else:
-        pylist = []
-    for arg in args:
-        pylist.append(arg)
+        pylistlist = []
+    for i in range(1, len(sys.argv)):
+        py.append(sys.argv[i])
     save_to_json_file(pylist, filename)
