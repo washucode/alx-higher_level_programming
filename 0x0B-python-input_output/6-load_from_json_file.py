0c1e74aaf1728b0ff7fb56c90b620697a0aa2d89
washucode <estherw8525@gmail.com>
2023-07-13 00:45:09 +0300

    reconfigure task 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,5 +6,5 @@ import json
 
 def load_from_json_file(filename):
     """Create object from a JSON file"""
-    with open(filename, mode="r", encoding="utf-8") as f:
-        return json.load(filename, f)
+    with open(filename) as f:
+        return json.load(f)
