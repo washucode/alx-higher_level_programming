2a004a0e7414147d51a92863f50c05af5fede45d
washucode <estherw8525@gmail.com>
2023-07-12 17:06:37 +0300

    make file executable

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" BaseGeometry Module """
+
+
+class BaseGeometry:
+    """ BaseGeometry class """
+    
+    def area(self):
+        """ Raise an Exception with the message area() is not implemented """
+        raise Exception("area() is not implemented")
+
+        
