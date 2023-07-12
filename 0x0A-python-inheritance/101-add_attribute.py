ec4b5a129193a2e31750559abc4d7d28229f7c84
washucode <estherw8525@gmail.com>
2023-07-12 23:55:44 +0300

    fix pyodestyle erros

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -2,6 +2,7 @@
 
 """ Module that contains the function add_attribute """
 
+
 def add_attribute(obj, name, value):
     """ Add a new attribute to an object if its possible """
     if hasattr(obj, "__dict__"):
