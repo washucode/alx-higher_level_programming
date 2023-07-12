57f87916088359db92a034eed11a430cfa02b9f9
washucode <estherw8525@gmail.com>
2023-07-13 00:59:00 +0300

    remove unused import refactor task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 """Add item module"""
-import json
 import sys
 from os import path
 
@@ -16,5 +15,5 @@ if __name__ == "__main__":
     else:
         pylistlist = []
     for i in range(1, len(sys.argv)):
-        py.append(sys.argv[i])
+        pylist.append(sys.argv[i])
     save_to_json_file(pylist, filename)
