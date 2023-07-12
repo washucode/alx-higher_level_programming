307439b1bbeab9b86e478a98d67f37bb86496a97
washucode <estherw8525@gmail.com>
2023-07-13 00:43:42 +0300

    task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Create object from a JSON file module"""
+import json
+
+
+def load_from_json_file(filename):
+    """Create object from a JSON file"""
+    with open(filename, mode="r", encoding="utf-8") as f:
+        return json.load(filename, f)
