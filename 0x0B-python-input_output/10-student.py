91984570d8e662e0e5cf797b8c187ca6e676bb78
washucode <estherw8525@gmail.com>
2023-07-13 01:12:21 +0300

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+"""Student to JSON module"""
+
+
+class Student:
+    """Class Student that defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes the data"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation"""
+        if (type(attrs) == list and
+                all(type(x) == str for x in attrs)):
+            return {x: getattr(self, x) for x in attrs if hasattr(self, x)}
+        return self.__dict__
