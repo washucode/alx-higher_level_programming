4668a7c65ab28a110d8b787ddfeaf25a565cd53f
washucode <estherw8525@gmail.com>
2023-07-20 21:22:24 +0300

    add getter  and setter for x and y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -79,3 +79,33 @@ class Rectangle(Base):
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
+
+    @property
+    def x(self):
+        """ Getter for x """
+        return self.__x
+    
+    @x.setter
+    def x(self, value):
+        """ Setter for x """
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """ Getter for y """
+        return self.__y
+    
+    @y.setter
+    def y(self, value):
+        """ Setter for y """
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+
