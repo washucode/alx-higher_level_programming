57b18cc545afa62474e76cf8d1afcd4c4361960a
washucode <estherw8525@gmail.com>
2023-08-16 03:34:50 +0300

    fix 11

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- script that lists all records with a score >= 10 script that lists all records with a score >= 10 
-SELECT score, name from second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
