f9e995aa47ff2ccb7b2c0d11292909894ecd77ca
washucode <estherw8525@gmail.com>
2023-08-17 23:44:32 +0300

    TASK 12

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,5 @@
+-- ists all shows contained in hbtn_0d_tvshows without a genre linked.
+SELECT t.title, g.genre_id FROM tv_shows t 
+LEFT JOIN tv_show_genres g ON t.id = g.show_id
+WHERE g.genre_id IS NULL
+ORDER BY t.title, g.genre_id ASC ;
