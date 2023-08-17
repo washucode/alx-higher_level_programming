336fed4c9167f59b339798f8edf189199962baff
washucode <estherw8525@gmail.com>
2023-08-17 23:44:24 +0300

    TASK 11

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,3 @@
+--  a script that lists all shows contained in the database hbtn_0d_tvshows
+SELECT t.title, g.genre_id FROM tv_shows t LEFT JOIN tv_show_genres g ON t.id = g.show_id
+ORDER BY t.title, g.genre_id ASC ;
