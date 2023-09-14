35c9b046659961da6ecbb9b607eaec2e1fb4f4c6
washucode <estherw8525@gmail.com>
2023-09-14 03:14:36 +0300

    task 3

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Script that prints the first argument passed to it:
+
+const args = process.argv.slice(2);
+if (args.length === 0) {
+  console.log('No argument');
+} else {
+  console.log(args[0]);
+}
