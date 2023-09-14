8633a21f909120d285b611cebc2b08d5d378ebd9
washucode <estherw8525@gmail.com>
2023-09-14 04:04:56 +0300

    task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// Script that adds a method charPrint(c)
+
+const Square1 = require('./5-square');
+
+class Square extends Square1 {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
