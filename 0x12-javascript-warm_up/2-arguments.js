47bed8e8b1de22fe20699b018e660f09f5810237
washucode <estherw8525@gmail.com>
2023-09-14 03:14:29 +0300

    task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Script that prints a message depending of the number of arguments passed:
+// If no arguments are passed to the script, print “No argument”
+// If only one argument is passed to the script, print “Argument found”
+// Otherwise, print “Arguments found”
+
+const args = process.argv.slice(2);
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
