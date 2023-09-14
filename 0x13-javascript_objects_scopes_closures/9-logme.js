bb73f3aba247d191e499aa867ab75b92bd424753
washucode <estherw8525@gmail.com>
2023-09-14 04:12:50 +0300

    task 9

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Script that prints the number of arguments already printed and the new argument value.
+
+let counter = 0;
+
+exports.logMe = function (item) {
+  console.log(`${counter}: ${item}`);
+  counter++;
+};
