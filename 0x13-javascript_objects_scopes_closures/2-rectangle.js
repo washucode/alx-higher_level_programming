a9b3bda54f02770984f2b0f9f78ecd304bd0a891
washucode <estherw8525@gmail.com>
2023-09-14 03:49:33 +0300

    task 2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Script that defines a rectangle with width and height
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
