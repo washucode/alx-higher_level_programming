0b2af63fe130ae4b3833d5b90a27d38da3ab69c7
washucode <estherw8525@gmail.com>
2023-09-23 03:23:26 +0300

    advance #1

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+Start link class to table in database
+"""
+
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker()
+    session = Session(bind=engine)
+
+    new_state = State(name='California')
+    new_city = City(name='San Francisco')
+    new_state.cities.append(new_city)
+    session.add(new_state)
+    session.add(new_city)
+    session.commit()
+
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Contains the class definition of a City. Improved
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from relationship_state import Base, State
+
+
+class City(Base):
+    """Class City that inherits from Base"""
+    # Defining the name of the table.
+    __tablename__ = 'cities'
+    # Defining the columns for the table.
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
