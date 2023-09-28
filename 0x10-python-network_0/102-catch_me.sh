92e0c63bd6d6ee597187f8aa797dd42a56b433b7
washucode <estherw8525@gmail.com>
2023-09-28 21:51:17 +0300

    task 9 corrections

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A bash script that sends a request to url and displays you got me!
-curl -o /dev/null -sw "You got me!"  0.0.0.0:5000/catch_me
+curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
