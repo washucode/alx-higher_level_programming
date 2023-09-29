ca84e02cba7df0cb6fe8c3aa17caf00e18c81cd2
washucode <estherw8525@gmail.com>
2023-09-29 13:37:56 +0300

    task 0 correction

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status"""
+"""
+    Fetches https://intranet.hbtn.io/status
+"""
 
-import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode("utf-8")))
+
+if __name__ == "__main__":
+    import urllib.request
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
