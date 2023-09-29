52e35972985b9c56e118429f94cc2e12530e5839
washucode <estherw8525@gmail.com>
2023-09-29 13:58:35 +0300

    task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+    Takes in a URL,displays the body of the response.
+    displayes the value of the X-Request-Id variable found in the header of the
+    response.
+"""
+
+import requests
+import sys
+
+
+def fetch_url(url):
+    """
+        Takes in a URL,displays the body of the response.
+        displayes the value of the X-Request-Id variable found in the header of
+        the response.
+
+        Args:
+            url : resource url
+    """
+    try:
+        r = requests.get(url)
+        print(r.headers['X-Request-Id'])
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    fetch_url(url)
