9adfc7a12f638e7a23a0233321de1a015206d7a7
washucode <estherw8525@gmail.com>
2023-10-24 13:45:32 +0300

    task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+/*
+    * Script that writes a string to a file.
+    * The first argument is the file path.
+    * The second argument is the string to write.
+    * The content of the file must be written in utf-8.
+    * If an error occurred during while writing, print the error object.
+*/
+
+const fs = require('fs');
+const file = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(file, content, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
